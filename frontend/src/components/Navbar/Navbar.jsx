ef9c84d76bcd63562bf471a859d7618dd2082ee3
Pratham Ranjan Singh <67726405+prathamranjansingh@users.noreply.github.com>
2024-07-12 14:45:25 +0530

    navbar fixed

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { StoreContext } from "../../context/StoreContext";
 import { useAuth0 } from "@auth0/auth0-react";
-import config from "../config";
+import config from "../../config";
 import axios from "axios";
 
 function Navbar() {
