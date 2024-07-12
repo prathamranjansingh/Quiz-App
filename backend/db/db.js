867b5bc53a3baf943bbc7298955edd757a5bf2ea
Pratham Ranjan Singh <67726405+prathamranjansingh@users.noreply.github.com>
2024-07-12 18:32:32 +0530

    db url updated

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose
-    .connect(process.env.DATABASE_URL)
+    .connect(
+      "mongodb+srv://prathamranjan3:H1IEw6OGtkmDk1mO@cluster0.nxuuszf.mongodb.net/QuizApp"
+    )
     .then(() => console.log("DB connected"));
 };
 
